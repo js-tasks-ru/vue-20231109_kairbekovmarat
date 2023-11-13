4ccafcaf428e823a74de84f835629e3c87624c30
Marat Kairbekov <kairbekovmarat@gmail.com>
2023-11-14 01:25:53 +0600

    Task completed

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,14 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const App = defineComponent({
+  name: 'App',
+
+  data() {
+    return {
+      count: 0,
+    }
+  }
+});
+
+const app = createApp(App);
+app.mount('#app');
