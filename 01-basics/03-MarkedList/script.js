70074cd1ad65c98e33617f1eda65a5d10fd0406a
Marat Kairbekov <kairbekovmarat@gmail.com>
2023-11-14 02:08:28 +0600

    Task 03-MarkedList completed

--- 01-basics/03-MarkedList/script.js
+++ 01-basics/03-MarkedList/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // From https://jsonplaceholder.typicode.com/comments
 const emails = [
@@ -29,4 +29,19 @@ const emails = [
   'Isaias_Kuhic@jarrett.net',
 ];
 
-// Требуется создать Vue приложение
+const app = createApp({
+  data() {
+    return {
+      emails,
+      search: '',
+    };
+  },
+
+  computed: {
+    markedEmails() {
+      return this.emails.filter(email => email.toLowerCase().indexOf(this.search.toLowerCase()) != -1);
+    }
+  }
+});
+
+app.mount('#app');
