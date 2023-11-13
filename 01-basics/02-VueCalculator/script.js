214cbac6cfd06361af6ca929ad25d3b19e166ca5
Marat Kairbekov <kairbekovmarat@gmail.com>
2023-11-14 01:47:10 +0600

    Task VueCalculator completed

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,34 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const app = createApp({
+  data() {
+    return {
+      numA: 0,
+      numB: 0,
+      operator: 'sum',
+    };
+  },
+
+  computed: {
+    calculatedNumber() {
+      switch(this.operator) {
+        case 'sum':
+          return this.numA + this.numB;
+
+        case 'subtract':
+          return this.numA - this.numB;
+
+        case 'multiply':
+          return this.numA * this.numB;
+
+        case 'divide':
+          return this.numA / this.numB;
+
+        default:
+          return NaN;
+      }
+    }
+  }
+});
+
+app.mount('#app');
