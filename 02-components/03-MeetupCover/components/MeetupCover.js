707021a72f3df2dc7de273d384fbe904213bd8c9
Marat Kairbekov <kairbekovmarat@gmail.com>
2023-11-27 02:07:39 +0600

    Task 03-MeetupCover fixed

--- 02-components/03-MeetupCover/components/MeetupCover.js
+++ 02-components/03-MeetupCover/components/MeetupCover.js
@@ -15,6 +15,8 @@ export default defineComponent({
 
   template: `
     <div class="meetup-cover" :style="image && \`--bg-url: url('\${image}')\`">
+        <!-- Ссылка выше - пример верстки при наличии изображения, а не изображение по умолчанию. -->
+        <!-- Изображение по умолчанию уже есть в стилях. Для его вывода достаточно не добавлять никаких стилей. -->
         <h1 class="meetup-cover__title">{{ title }}</h1>
     </div>`,
 });
