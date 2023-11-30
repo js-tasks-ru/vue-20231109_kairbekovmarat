3f31abd92092c1edfa1e2e2f89ff820663d112e2
Marat Kairbekov <kairbekovmarat@gmail.com>
2023-11-30 16:09:35 +0600

    Task 02-TheToaster completed

--- 04-vue-cli/02-TheToaster/components/TheToaster.vue
+++ 04-vue-cli/02-TheToaster/components/TheToaster.vue
@@ -1,24 +1,40 @@
 <template>
   <div class="toasts">
-    <div class="toast toast_success">
-      <UiIcon class="toast__icon" icon="check-circle" />
-      <span>Success Toast Example</span>
-    </div>
-
-    <div class="toast toast_error">
-      <UiIcon class="toast__icon" icon="alert-circle" />
-      <span>Error Toast Example</span>
-    </div>
+    <Toast v-for="toast in toasts"
+      :message="toast.message"
+      :type="toast.type" />
   </div>
 </template>
 
 <script>
-import UiIcon from './UiIcon.vue';
+import Toast from './Toast.vue';
 
 export default {
   name: 'TheToaster',
 
-  components: { UiIcon },
+  data() {
+    return {
+      toasts: [],
+    }
+  },
+
+  methods: {
+    success(text) {
+      this.toasts.push({
+        message: text,
+        type: 'success',
+      });
+    },
+
+    error(text) {
+      this.toasts.push({
+        message: text,
+        type: 'error',
+      });
+    }
+  },
+
+  components: { Toast }
 };
 </script>
 
@@ -39,35 +55,5 @@ export default {
     bottom: 72px;
     right: 112px;
   }
-}
-
-.toast {
-  display: flex;
-  flex: 0 0 auto;
-  flex-direction: row;
-  align-items: center;
-  padding: 16px;
-  background: #ffffff;
-  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.15);
-  border-radius: 4px;
-  font-size: 18px;
-  line-height: 28px;
-  width: auto;
-}
-
-.toast + .toast {
-  margin-top: 20px;
-}
-
-.toast__icon {
-  margin-right: 12px;
-}
-
-.toast.toast_success {
-  color: var(--green);
-}
-
-.toast.toast_error {
-  color: var(--red);
 }
 </style>
